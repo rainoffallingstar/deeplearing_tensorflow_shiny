14e6160ef6bfe1a682270369c46d38d3e1be237a
ubuntu <ubuntu@localhost.localdomain>
2022-12-31 16:36:29 +0800

    update the app

--- app.R
+++ app.R
@@ -1,11 +1,18 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#####
+# 说明
+# 文件构架：
+#- app.R
+#|- data (默认调用本地文件夹名，内应含已处理好的train、validatation文件夹)
+#|-- train （按文件夹名分类放置）
+#|-- validation
+#|-- test_{分类名} （默认调用测试文件夹序列，内置默认全分类的空文件夹，及测试对象的文件）
+#|- model （默认保存和调用的训练模型文件，以模型名称命名文件夹）
+# workflow
+# 1. 随机划分测试集患者序列
+# 2. 按标签汇总文件，并进行训练集和测试集的增强
+# 3. 设置训练过程，epoch设置为30
+# 4. 记录结果
+
 library(keras)
 library(tfhub)
 library(tfdatasets)
@@ -16,8 +23,9 @@ library(pins)
 library(fs)
 library(shinyWidgets)
 library(shiny)
+library(pROC)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
@@ -28,7 +36,7 @@ ui <- fluidPage(
         sidebarPanel(
           textInput("filedir",
                     "input your task path",
-                    "dataset"),
+                    "data"),
           sliderInput("dropout_factor", 
                       "Set a probability for dropout layer",
                       min = 0,
@@ -36,9 +44,9 @@ ui <- fluidPage(
                       value = 0.2),
             sliderInput("epoch",
                         "Number of epoch",
-                        min = 1,
+                        min = 0,
                         max = 100,
-                        value = 3),
+                        value = 30),
           pickerInput("model",
                       "Select a model:",
                       selected = "plainCNN",
@@ -59,7 +67,9 @@ ui <- fluidPage(
              # "训练细节", verbatimTextOutput("history")
             #),
             tabPanel(
-              "测试结果", plotOutput("predict")
+              "测试结果", plotOutput("predict"),
+              verbatimTextOutput("aucvalue")
+              
             )
             #tabPanel(
               #"ROC图", plotOutput("rocplots")
@@ -77,7 +87,7 @@ server <- function(input, output) {
   classnum <- reactive({
     classes <- list.dirs(paste0(getwd(), "/", input$filedir, "/train/"),
                          full.names = FALSE, recursive = FALSE)
-    length(classes)
+    
   })
   
   
@@ -119,18 +129,8 @@ server <- function(input, output) {
     )
   })
   
-  test_flow <- reactive({
-    flow_images_from_directory(
-      generator = validation_image_gen(),
-      directory = paste0(getwd(), "/", input$filedir, "/test/"), 
-      target_size = c(224, 224),
-      class_mode = NULL,
-      shuffle = FALSE
-    )
-  }) 
-  
   models <- reactive({
-    nums <- classnum()
+    nums <- length(classnum())
     if (input$model == "mobilenet") {
       mob <- application_mobilenet(include_top = FALSE, pooling = "avg")
       unfreeze_weights(mob)
@@ -183,13 +183,13 @@ server <- function(input, output) {
   output$plots <- renderPlot({
     training_image_flow <- training_image_flow()
     validation_image_flow <- validation_image_flow()
-    models <- model()
+    model <- models()
     withProgress(message = 'Calculation in progress',
                  detail = 'This may take a while...', value = 0, {
     model %>% 
       compile(loss = "categorical_crossentropy", optimizer = "adam", metrics = "accuracy")
     # Include the epoch in the file name
-    checkpoint_path <- paste0(getwd(),"/",input$model, 
+    checkpoint_path <- paste0(getwd(),"/model/",input$model, 
                               "/cp-list{epoch:04d}.ckpt")
     checkpoint_dir <- fs::path_dir(checkpoint_path)
     # Create a callback that saves the model's weights every 2 epochs
@@ -212,16 +212,88 @@ server <- function(input, output) {
     plot(history)
     
   })
+  
+  predict_result <- reactive({
+    checkpoint_path <- paste0(getwd(),"/model/",input$model, 
+                              "/cp-list{epoch:04d}.ckpt")
+    checkpoint_dir <- fs::path_dir(checkpoint_path)
+    latest <- tf$train$latest_checkpoint(checkpoint_dir)
+    model <- models()
+    load_model_weights_tf(model,latest)
+    
+    cla <- classnum()
+    df <- data.frame()
+    for (i in 1:length(cla)) {
+      test_flow <-  flow_images_from_directory(
+        generator = image_data_generator(),
+        directory =  paste0(getwd(), "/", input$filedir, "/test_",
+                            cla[i],"/"), 
+        target_size = c(224, 224),
+        class_mode = "categorical",
+        shuffle = FALSE
+      )
+      d <- model %>% 
+        predict(test_flow, steps = test_flow$n/test_flow$batch_size) 
+      if (i == 1){
+        result <- data.frame(rep(cla[i],length(d[,i])), d[,i]) 
+      }else{
+        result <- data.frame(rep(cla[i],length(d[,i])), 1-d[,i])
+      } 
+      df <- rbind(df,result)
+      rocobj <- roc(
+        df[,1], 
+        df[,2],
+        smooth = F
+      )
+    }
+  })
 
   
-  output$predict <- renderPrint({
-    test_flow <- test_flow()
-    traincnn() %>% predict_generator(
-        test_flow,
-        steps = test_flow$n/test_flow$batch_size
-      )
-      
+  output$predict <- renderPlot({
+    rocobj <- predict_result()
+    # 计算临界点/阈值
+    cutOffPoint <- coords(rocobj, "best")
+    cutOffPointText <- paste0(round(cutOffPoint[1],3),"(",round(cutOffPoint[2],3),",",round(cutOffPoint[3],3),")")
+    
+    # 计算AUC值
+    auc<-auc(rocobj)[1]
+    # AUC的置信区间
+    auc_low<-ci(rocobj,of="auc")[1]
+    auc_high<-ci(rocobj,of="auc")[3]
+    
+    # 计算置信区间
+    ciobj <- ci.se(rocobj,specificities=seq(0, 1, 0.01))
+    data_ci<-ciobj[1:101,1:3]
+    data_ci<-as.data.frame(data_ci)
+    x=as.numeric(rownames(data_ci))
+    data_ci<-data.frame(x,data_ci)
+    
+    # 绘图
+    ggroc(rocobj,
+          color="red",
+          size=1,
+          legacy.axes = F # FALSE时 横坐标为1-0 specificity；TRUE时 横坐标为0-1 1-specificity
+    )+
+      theme_classic()+
+      geom_segment(aes(x = 1, y = 0, xend = 0, yend = 1),        # 绘制对角线
+                   colour='grey', 
+                   linetype = 'dotdash'
+      ) +
+      geom_ribbon(data = data_ci,                                # 绘制置信区间
+                  aes(x=x,ymin=X2.5.,ymax=X97.5.), 
+                  fill = 'lightblue',
+                  alpha=0.5)+
+      geom_point(aes(x = cutOffPoint[[2]],y = cutOffPoint[[3]]))+ # 绘制临界点/阈值
+      geom_text(aes(x = cutOffPoint[[2]],y = cutOffPoint[[3]],label=cutOffPointText),vjust=-1) # 添加临界点/阈值文字标签
+    
   })
+  
+  output$aucvlue <- renderPrint({
+    rocobj <- predict_result()
+    auc<-auc(rocobj)[1]
+  }
+    
+  )
  
  
 }
--- figure/README.md
+++
@@ -1,1 +0,0 @@
-> this folder is used to store all imgs produced by our project and all the necessary imgs applied in the manuscript
